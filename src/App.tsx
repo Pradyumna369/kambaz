8df5822f0d69f0a1da4e09126b5aa8892e16b086
Pradyumna Nunna <156878599+Pradyumna369@users.noreply.github.com>
2025-02-05 16:18:40 -0500

    HTML elements
    Added required html elements of assignment 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,22 @@
+import { HashRouter,Route,Routes,Navigate } from "react-router";
+import Lab1 from "./Labs/Lab1";
+import Lab2 from "./Labs/Lab2";
+import Labs from "./Labs";
+import Kambaz from "./kambaz";
 function App() {
-  return <h1>Welcome to Web Development</h1>;
+  return (
+    <HashRouter>
+  <div>
+    <Routes>
+      <Route path="/" element={<Navigate to="/Labs" />} />
+      <Route path="Labs" element={<Labs/>}/>
+      <Route path="Labs/Lab1" element={<Lab1 />}/>
+      <Route path="Labs/Lab2" element={<Lab2 />}/>
+      <Route path="/Kambaz/*" element={<Kambaz/>}  />
+    </Routes>
+  </div>
+  </HashRouter>
+  );
 }
 
 export default App;

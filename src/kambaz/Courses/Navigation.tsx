2f48a18ca6c7030263843c1f66a61c6bd3c4d7c3
Pradyumna Nunna <156878599+Pradyumna369@users.noreply.github.com>
2025-02-27 23:03:26 -0500

    Data driven components
    Data driven navigation, dashboard, course screen, course navigation

--- src/kambaz/Courses/Navigation.tsx
+++ src/kambaz/Courses/Navigation.tsx
@@ -1,20 +1,20 @@
-import { Link } from "react-router-dom";
+import { Link, useParams, useLocation } from "react-router-dom";
+import { courses } from "../Database";
+
 export default function CourseNavigation() {
-return (
-<div id="wd-courses-navigation" className="wd list-group fs-5 rounded-0">
-<Link to="/Kambaz/Courses/1234/Home" id="wd-course-home-link"
-className="list-group-item active border border-0"> Home </Link> 
-<Link to="/Kambaz/Courses/1234/Modules" id="wd-course-modules-link"
-className="list-group-item text-danger border border-0"> Modules </Link> 
-<Link to="/Kambaz/Courses/1234/Piazza" id="wd-course-piazza-link"
-className="list-group-item text-danger border border-0"> Piazza </Link> 
-<Link to="/Kambaz/Courses/1234/Zoom" id="wd-course-zoom-link"
-className="list-group-item text-danger border border-0"> Zoom </Link> 
-<Link to="/Kambaz/Courses/1234/Assignments" id="wd-course-quizzes-link"
-className="list-group-item text-danger border border-0"> Assignments </Link> 
-<Link to="/Kambaz/Courses/1234/Quizzes" id="wd-course-assignments-link"
-className="list-group-item text-danger border border-0"> Quizzes </Link> 
-<Link to="/Kambaz/Courses/1234/People" id="wd-course-people-link"
-className="list-group-item text-danger border border-0" > People </Link> 
-</div>
-);}
+    const { cid } = useParams();
+    const {pathname} = useLocation();
+    const links = ["Home", "Modules", "Piazza", "Zoom", "Assignments", "Quizzes", "Grades", "People"];
+return (    
+    <div id="wd-courses-navigation" className="wd list-group fs-5 rounded-0">
+    {
+        links.map((link) => (
+            <Link to={`/Kambaz/Courses/${cid}/${link}`} id="wd-course-home-link"
+            className={`list-group-item  
+            ${pathname.includes(link)?"active border border-0 text-black ":"border border-0 text-danger"}`}> 
+            {link} </Link>
+        ))
+    }
+    </div>
+    );
+}

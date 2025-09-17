42b636b4d03eb5eadd2fe097403531b5afbfa8cf
Pradyumna Nunna <156878599+Pradyumna369@users.noreply.github.com>
2025-09-16 20:23:18 -0400

    lab4

---
+++ src/Labs/Lab4/ReduxExamples/todos/TodoItem.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import {useDispatch} from "react-redux";
+import {deleteTodo, setTodo} from "./todosReducer";
+
+export default function TodoItem({todo}) {
+        const dispatch = useDispatch();
+        return (
+            <li key = {todo.id}>
+                <button onClick = {() => dispatch(deleteTodo(todo.id))}
+                >Delete</button>
+                <button onClick = {() => dispatch(setTodo(todo))}>
+                    Edit   
+                </button> <></>  
+                {todo.title}
+            </li>
+        );
+}

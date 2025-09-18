2121ced5626f78b6b611c69facb163ed5f3ab313
Pradyumna Nunna <156878599+Pradyumna369@users.noreply.github.com>
2025-09-18 00:11:57 -0400

    account navigation
    added redux to account navigation

--- src/kambaz/Account/Navigation.tsx
+++ src/kambaz/Account/Navigation.tsx
@@ -1,9 +1,26 @@
-import { Link } from "react-router-dom";
+import { Link, NavLink, useLocation } from "react-router-dom";
+import { useSelector } from "react-redux";
+import ProtectedRoute from './ProtectedRoute'
+
 export default function AccountNavigation() {
+  const { currentUser } = useSelector((state) => state.accountReducer);
+  const links = currentUser ? ["Profile"] : ["Signin", "Signup"];
+  const {pathname} = useLocation();
   return (
-    <div id="wd-account-navigation">
-      <Link to={`/Kambaz/Account/Signin`}  > Signin  </Link> <br/>
-      <Link to={`/Kambaz/Account/Signup`}  > Signup  </Link> <br/>
-      <Link to={`/Kambaz/Account/Profile`} > Profile </Link> <br/>
+    <div id="wd-account-navigation" className = "list-group">
+      {links.map((link) => (
+        <NavLink
+        id="wd-account-signin-link"
+        to={`/Kambaz/Account/${link}`}
+        className={({ isActive }) =>
+          `list-group-item border border-0 ${
+            isActive ? "active" : "text-danger"
+          }`
+        }
+      >
+        {link}
+      </NavLink>
+      ))}
+
     </div>
 );}

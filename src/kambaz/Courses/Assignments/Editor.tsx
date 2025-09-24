d82b810d32e9c94d0798b7d9fb42158edd6a41af
Pradyumna Nunna <156878599+Pradyumna369@users.noreply.github.com>
2025-09-24 00:12:55 -0400

    assignments
    able to add, edit the assignments and delete assignment after getting confirmation from a dialog box pop up

--- src/kambaz/Courses/Assignments/Editor.tsx
+++ src/kambaz/Courses/Assignments/Editor.tsx
@@ -1,12 +1,66 @@
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate, useParams } from "react-router";
+import { useEffect, useState } from "react";
+import { updateAssignment, addAssignment } from "./reducer";
+
 export default function AssignmentEditor() {
+    const { cid, aid } = useParams();
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+
+    const assignment = useSelector((state:any) => 
+      state.assignmentsReducer.assignments.find((a:any) => a._id == aid)
+    );
+    const [title, setTitle] = useState(assignment?.title || "");
+    const [description, setDescription] = useState(assignment?.description || "");
+    const [availableFrom, setAvailableFrom] = useState(assignment?.availableFrom || "");
+    const [dueDate, setDueDate] = useState(assignment?.dueDate || "");
+    const [availableUntil, setAvailableUntil] = useState(assignment?.availableUntil || "");
+    const [points, setPoints] = useState(assignment?.points || "");
+    const [selectedOption, setSelectedOption] = useState("");
+
+    useEffect(() => {
+      if(assignment) {
+        setTitle(assignment.title);
+        setDescription(assignment.description);
+        setPoints(assignment.points);
+        setDueDate(assignment.dueDate);
+        setAvailableFrom(assignment.availableFrom);
+        setAvailableUntil(assignment.availableUntil);
+      } 
+    },[assignment]);
+
+    const handleSubmissionTypeChange = 
+      (event : React.ChangeEvent<HTMLSelectElement>) => {
+        setSelectedOption(event.target.value);
+      }
+    
+    const handleSave = () => {
+      const updatedAssignment = {
+        _id : assignment?._id || new Date().getTime().toString(),
+        title,
+        description,
+        points,
+        dueDate,
+        availableFrom,
+        availableUntil,
+        course: cid, 
+      };
+
+      if(assignment) {
+        dispatch(updateAssignment(updatedAssignment));
+      } else {
+        dispatch(addAssignment(updatedAssignment))
+      }
+      navigate(-1);
+    }
+
     return (
       <div id="wd-assignments-editor">
         <label htmlFor="wd-name">Assignment Name</label>
-        <input id="wd-name" value="A1 - ENV + HTML" /><br /><br />
-        <textarea id="wd-description">
-          The assignment is available online Submit a link to the landing page of your Web Application
-          running on Netlify. The landing page should include the following: Your full name and section
-          Links to each of the lab assignments
+        <input id="wd-name" value={title} onChange={(e) => setTitle(e.target.value)}/><br /><br />
+        <textarea id="wd-description" onChange={(e) => setDescription(e.target.value)}>
+          {description}
         </textarea>
         <br />
         <table>
@@ -15,7 +69,7 @@ export default function AssignmentEditor() {
             <label htmlFor="wd-points">Points</label>
           </td>
           <td>
-            <input id="wd-points" value={100} />
+            <input id="wd-points" value={points} onChange={(e) => setPoints(Number(e.target.value))}/>
           </td>
         </tr><br />
         <tr>
@@ -25,8 +79,11 @@ export default function AssignmentEditor() {
           <td>
           <form >
             <select name="Assignment Group" id="wd-group">
+                <option value="">--select--</option>
                 <option value="Assignments">ASSIGNMENTS</option>
-                <option value="some">Publish modules only</option>
+                <option value="quiz">Quiz</option>
+                <option value="exam">Exam</option>
+                <option value="project">Project</option>
             </select>
         </form>
           </td>
@@ -38,6 +95,7 @@ export default function AssignmentEditor() {
           <td>
           <form >
             <select name="Percentage" id="wd-display-grade-as">
+                <option value="">--select--</option>
                 <option value="Percentage">Percentage</option>
                 <option value="some">Example</option>
             </select>
@@ -50,9 +108,10 @@ export default function AssignmentEditor() {
           </td>
           <td>
           <form >
-            <select name="Submission" id="wd-submission-type">
+            <select name="Submission" id="wd-submission-type" onChange={handleSubmissionTypeChange}>
+                <option value="">--select--</option>
                 <option value="Online">Online</option>
-                <option value="some">Example</option>
+                <option value="in-person">In Person</option>
             </select>
         </form>
           </td>
@@ -94,7 +153,8 @@ export default function AssignmentEditor() {
           <td>
             <form>
               <label htmlFor="wd-due-date">Due</label><br />
-              <input type="date" name="Due Date" id="wd-due-date" />
+              <input type="date" name="Due Date" id="wd-due-date" value={dueDate} 
+              onChange={(e) => setDueDate(e.target.value)}/>
             </form>
           </td>
         </tr><br />
@@ -104,13 +164,16 @@ export default function AssignmentEditor() {
           <td>
             <form>
               <label htmlFor="Available" id="wd-available-from">Available from</label><br />
-              <input type="date" id="wd-available-from" name="Available Date"/>
+              <input type="date" id="wd-available-from" name="Available Date" value={availableFrom}
+              onChange={(e) => {setAvailableFrom(e.target.value)}}
+              />
             </form>
           </td>
           <td>
           <form>
               <label htmlFor="Until" id="wd-available-until">Until</label><br />
-              <input type="date" id="wd-available-until" name="Until Date" value="2025-04-26"/>
+              <input type="date" id="wd-available-until" name="Until Date" value={availableUntil} 
+              onChange={(e) => {setAvailableUntil(e.target.value)}}/>
             </form>
           </td>
         </tr>
@@ -119,11 +182,10 @@ export default function AssignmentEditor() {
       </table>
       <hr />
       <div style={{ textAlign: "right" }}>
-      <button>
+      <button onClick={() => navigate(-1)}>
         Cancel
       </button>
-      
-      <button>
+      <button onClick={handleSave}>
         Save
       </button>
       </div>
